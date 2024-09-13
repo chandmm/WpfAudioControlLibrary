251208732f1047ef4c8d2b043295b67f697b7ff3
ChandMM <michael.mchand@gmail.com>
2024-09-14 02:11:30 +1200

    Code update - VU control implementation - Work in progress

--- WpfAudioControlLibrary/Controls/VUControl.xaml.cs
+++ WpfAudioControlLibrary/Controls/VUControl.xaml.cs
@@ -9,6 +9,12 @@ namespace WpfAudioControlLibrary.Controls
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        #region Fields
+
+        
+
+        #endregion
+
         #region Dependency Properties
 
         public static readonly DependencyProperty MinimumProperty =
@@ -44,12 +50,74 @@ namespace WpfAudioControlLibrary.Controls
             set => SetValue(DataProperty, value);
         }
 
+        public static readonly DependencyProperty Mark1Property =
+            DependencyProperty.Register("Mark1", typeof(string), typeof(VUControl), new PropertyMetadata("0", null));
+        public string Mark1
+        {
+            get => (string)GetValue(Mark1Property);
+            set => SetValue(Mark1Property, value);
+        }
+
+        public static readonly DependencyProperty Mark2Property =
+            DependencyProperty.Register("Mark2", typeof(string), typeof(VUControl), new PropertyMetadata("0", null));
+        public string Mark2
+        {
+            get => (string)GetValue(Mark2Property);
+            set => SetValue(Mark2Property, value);
+        }
+
+        public static readonly DependencyProperty Mark3Property =
+            DependencyProperty.Register("Mark3", typeof(string), typeof(VUControl), new PropertyMetadata("0", null));
+        public string Mark3
+        {
+            get => (string)GetValue(Mark3Property);
+            set => SetValue(Mark3Property, value);
+        }
+
+        public static readonly DependencyProperty Mark4Property =
+            DependencyProperty.Register("Mark4", typeof(string), typeof(VUControl), new PropertyMetadata("0", null));
+        public string Mark4
+        {
+            get => (string)GetValue(Mark4Property);
+            set => SetValue(Mark4Property, value);
+        }
+
+        #endregion
+
+        #region Member Properties
+
+        private double _gridWidth;
+        public double GridWidth
+        {
+            get => _gridWidth;
+            set
+            {
+                _gridWidth = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private double _gridHeight;
+        public double GridHeight
+        {
+            get => _gridHeight;
+            set
+            {
+                _gridHeight = value;
+
+                OnPropertyChanged();
+            }
+        }
+
         #endregion
 
         #region Initialisation
 
         public VUControl()
         {
+            DataContext = this;
+
             InitializeComponent();
         }
 
