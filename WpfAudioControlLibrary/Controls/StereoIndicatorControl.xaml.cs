7121dd968e269d58e36f866bfa783429e2b87aed
ChandMM <michael.mchand@gmail.com>
2024-09-20 20:49:38 +1200

    Added mono and stereo indicator lamps.

---
+++ WpfAudioControlLibrary/Controls/StereoIndicatorControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿/*
+   WPF Audio Control Library: Set if controls applicable to audio applications
+    Copyright (C) 2024  Michael Chand
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAudioControlLibrary.Controls
+{
+    public partial class StereoIndicatorControl : UserControl
+    {
+        public static readonly DependencyProperty IsMonoProperty =
+            DependencyProperty.Register("IsMono", typeof(bool), typeof(StereoIndicatorControl),
+                new PropertyMetadata(false, null));
+        public bool IsMono
+        {
+            get { return (bool)GetValue(IsMonoProperty); }
+            set { SetValue(IsMonoProperty, value); }
+        }
+
+        public StereoIndicatorControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
